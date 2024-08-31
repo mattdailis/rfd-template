02970bf00bdf6a5046bdfea2e706cba58b1b6851
Matthew Dailis <matthew.l.dailis@jpl.nasa.gov>
2024-08-30 20:21:45 -0700

    Include git checkout in new.sh

--- scripts/new.sh
+++ scripts/new.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-NEW_RFD_DIR=rfd/$1
+RFD_NUMBER=$1
+NEW_RFD_DIR=rfd/$RFD_NUMBER
 TITLE=$2
 NEW_RFD_FILE=$NEW_RFD_DIR/README.adoc
+
+git checkout -b $RFD_NUMBER
 
 mkdir $NEW_RFD_DIR
 cp prototypes/prototype.adoc $NEW_RFD_FILE
 sed -i '' -e "s/__TITLE__/$2/g" $NEW_RFD_FILE
-
