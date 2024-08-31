76ea7f4e3d5f0c7e6d43ccdc78590e00f2349a0b
Matthew Dailis <matthew.l.dailis@jpl.nasa.gov>
2024-08-30 20:36:02 -0700

    Update placeholder and new.sh script
    david-crespo has graciously provided the template RFD

--- scripts/new.sh
+++ scripts/new.sh
@@ -4,9 +4,15 @@ RFD_NUMBER=$1
 NEW_RFD_DIR=rfd/$RFD_NUMBER
 TITLE=$2
 NEW_RFD_FILE=$NEW_RFD_DIR/README.adoc
+BRANCH_NAME=$RFD_NUMBER
 
-git checkout -b $RFD_NUMBER
+git checkout -b $BRANCH_NAME
 
 mkdir $NEW_RFD_DIR
 cp prototypes/prototype.adoc $NEW_RFD_FILE
-sed -i '' -e "s/__TITLE__/$2/g" $NEW_RFD_FILE
+sed -i '' -e "s/<Number>/$RFD_NUMBER/g" $NEW_RFD_FILE
+sed -i '' -e "s/<Title>/$TITLE/g" $NEW_RFD_FILE
+
+git add $NEW_RFD_FILE
+git commit -m "$RFD_NUMBER: Adding placeholder for RFD \"$TITLE\""
+git push origin $BRANCH_NAME
